38c4586be5d9afc1b977e870d7d971dfbe39252c
Austin Fendley <austinfendley@tutanota.com>
2024-08-02 14:45:26 +0300

    Create variables.tf

---
+++ terraform/vultr-dev/variables.tf
@@ -0,0 +1,11 @@
+variable "digitalocean_token" {
+  description = "DigitalOcean API token"
+  type        = string
+  default     = ""
+}
+
+variable "VULTR_API_TOKEN" {
+  description = "Vultr API key"
+  type        = string
+  default     = ""
+}
